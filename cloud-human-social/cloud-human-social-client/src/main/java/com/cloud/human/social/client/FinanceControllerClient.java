15329ac96e622d3a953dbe9ca3fdd8d117f0231e
wangyi <wangyi23@longfor.com>
2019-08-09 18:22:23 +0800

    cloud-human-social

--- cloud-human-social/cloud-human-social-client/src/main/java/com/cloud/human/social/client/FinanceControllerClient.java
+++ cloud-human-social/cloud-human-social-client/src/main/java/com/cloud/human/social/client/FinanceControllerClient.java
@@ -3,7 +3,7 @@ package com.cloud.human.social.client;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "cloud-financa")
+@FeignClient(value = "/cloud-finance")
 public interface FinanceControllerClient {
     @GetMapping("/humanSocial/humanInfo")
     String getFinanceInfo();
