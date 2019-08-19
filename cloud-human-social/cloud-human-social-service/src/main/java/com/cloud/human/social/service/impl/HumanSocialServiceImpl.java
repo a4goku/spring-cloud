4fbb86274f6f6367be08d0609749de498eb09a93
wangyi <wangyi23@longfor.com>
2019-08-19 18:02:43 +0800

    add ignore

--- cloud-human-social/cloud-human-social-service/src/main/java/com/cloud/human/social/service/impl/HumanSocialServiceImpl.java
+++ cloud-human-social/cloud-human-social-service/src/main/java/com/cloud/human/social/service/impl/HumanSocialServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cloud.human.social.service.impl;
 
-import com.cloud.human.social.manager.HumanSocialManager;
 import com.cloud.human.social.service.HumanSocialService;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +8,13 @@ import javax.annotation.Resource;
 
 @Service
 public class HumanSocialServiceImpl implements HumanSocialService {
-    @Resource
-    private HumanSocialManager humanSocialManager;
+//    @Resource
+//    private HumanSocialManager humanSocialManager;
 
     @Override
     public String getFinanceInfo(){
-        return humanSocialManager.getFinanceInfo();
+        return "temp";
+        //return humanSocialManager.getFinanceInfo();
     }
 
 }
