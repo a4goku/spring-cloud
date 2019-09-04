6bf4200c7758d918a7f5121e5275b84f017adc71
wangyi <wangyi23@longfor.com>
2019-09-04 11:26:01 +0800

    用consul做注册中心

--- cloud-finance/cloud-finance-web/src/main/java/com/cloud/finance/Application.java
+++ cloud-finance/cloud-finance-web/src/main/java/com/cloud/finance/Application.java
@@ -3,11 +3,19 @@ package com.cloud.finance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@RestController
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "Hello World";
     }
 }
