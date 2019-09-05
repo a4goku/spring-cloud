b139014fb1ffef5d037157a399151a44368d7c21
wangyi <wangyi23@longfor.com>
2019-09-05 19:00:56 +0800

    调通调用

--- cloud-human-social/cloud-human-social-web/src/main/java/com/cloud/human/social/Application.java
+++ cloud-human-social/cloud-human-social-web/src/main/java/com/cloud/human/social/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 @EnableFeignClients
 public class Application {
     public static void main(String[] args) {
